c82181b8cc3712a4165bf5c10d3e7f4cf463ef16
veyselakbalik <veyselakbalik@ogr.iu.edu.tr>
2023-02-25 00:47:53 +0300

    Create collectionview ui

---
+++ PomodoroApp/Controllers/ToDoTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoTableViewController.swift
+//  PomodoroApp
+//
+//  Created by Veysel Akbalık on 25.02.2023.
+//
+
+import UIKit
+
+class ToDoTableViewController: UIViewController {
+
+    // MARK: - UI Elements
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+    
+    // MARK: - Functions
+    
+    // MARK: - Actions
+
+}
